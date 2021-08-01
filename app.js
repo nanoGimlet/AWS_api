fa21953de5f7d412b782e2bf8a6b571a93637a24
nanoGimlet <gota-nano@akaen.waseda.jp>
2021-08-02 00:28:23 +0900

    restapi base

--- app.js
+++ app.js
@@ -47,4 +47,41 @@ app.get('/secret',
   }
 );
 
+// 在庫の更新、作成
+// 在庫にない場合は新しく作成、在庫にある場合は数を追加
+// localhost/v1/stocks/:nameに返す
+app.post('/v1/stocks', (req, res) => {
+  
+});
+
+// 在庫チェック1
+// /v1/stocks/:nameの場合はそのname, amountを返す
+app.get('/v1/stocks/:name', (req, res) => {
+  
+});
+
+// 在庫チェック2
+// /v1/stocksの場合は全てのname, amountを返す
+app.get('/v1/stocks', (req, res) => {
+
+});
+
+// 販売
+// /v1/salesにアクセス、salesに計算値を加算, amountを減算
+app.post('/v1/stocks/:name', (req, res) => {
+
+});
+
+// 売り上げチェック
+// salesの値を返す
+app.get('/v1/sales', (req, res) => {
+
+});
+
+// 全削除
+// 空を返す
+app.delete('/v1/stocks', (req, res) => {
+
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
